79e7b374c3be64467305337518852ca1758f23b4
Vadim Persidskii <vadim@example.com>
2024-11-13 20:38:50 +0300

    Add both server and client

---
+++ src/main/java/Client.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Client");
+        try (Socket clientSocket = new Socket("localhost", Server.SERVER_PORT);
+             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            writer.println("John Smith");
+            System.out.println("Server replied: " + reader.readLine());
+        } catch(IOException | IllegalArgumentException e) {
+            System.out.println("ERROR Client failure: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/Server.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static final int SERVER_PORT = 8080;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            System.out.println("Server started successfully");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    final String name = in.readLine();
+                    System.out.println("New connection accepted. Got '" + name + "' from port " + clientSocket.getPort() );
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                } catch (IOException e) {
+                    System.out.println("ERROR Server failed to process client connection: " + e.getMessage());
+                }
+            }
+        } catch (IOException | IllegalArgumentException e) {
+            System.out.println("ERROR Server failure: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
